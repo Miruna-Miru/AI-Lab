11c0171ecb0233266a6cb1a0f25d714691332359
Miruna-Miru <mirmirunalini@gmail.com>
2024-09-30 21:29:13 +0530

    Added files

---
+++ try.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+plt.plot(x, y)
+plt.title('Line Plot')
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+
+xx = [ 2, 3, 4, 5]
+yy = [2,  5, 7, 5]
+plt.plot(xx, yy)
+
+
+plt.show()
+
+
