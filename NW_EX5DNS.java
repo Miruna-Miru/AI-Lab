52f3f45402b46905d688186d3a302f160ea3a764
Miruna-Miru <mirmirunalini@gmail.com>
2024-10-13 16:29:28 +0530

    updated dns

--- NW_EX5DNS.java
+++ NW_EX5DNS.java
@@ -1,25 +1,51 @@
+import java.util.Scanner;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 
 public class NW_EX5DNS {
+
     public static void main(String[] args) {
-        try {
-            
-            InetAddress localHost = InetAddress.getLocalHost();
-            System.out.println("Local Host IP: " + localHost.getHostAddress());
-            System.out.println("Local Host Name: " + localHost.getHostName());
+        Scanner inp = new Scanner(System.in);
+        resolveLocalDNS();
+        System.out.print("Enter domain nem : ");
+        String dn = inp.next();
+        resolveDomain(dn);
+    }
 
-      
-            String domainName = "google.com"; 
-            InetAddress[] inetAddresses = InetAddress.getAllByName(domainName);
-
-            System.out.println("IP Addresses for domain " + domainName + ": ");
-            for (InetAddress addr : inetAddresses) {
-                System.out.println(addr.getHostAddress());
+    static void resolveLocalDNS()
+    {
+        try
+        {
+            Enumeration<NetworkInterface> nwIfs= NetworkInterface.getNetworkInterfaces();
+            while (nwIfs.hasMoreElements()) 
+            {
+               NetworkInterface nw = nwIfs.nextElement();
+               Enumeration<InetAddress> inet = nw.getInetAddresses();
+               while(inet.hasMoreElements())
+               {
+                InetAddress ip = inet.nextElement();
+                System.out.println("Interface   :  "+nw.getName());
+                System.out.println("IP          :  "+ip.getHostAddress());
+                System.out.println("Host name :  "+ip.getHostName());
+               }    
             }
-        } catch (UnknownHostException e) {
-            System.out.println("Error resolving domain: " + e.getMessage());
+        }
+        catch(Exception e){
+        System.out.println(e.getMessage());
+        }
+    }
+
+  static  void resolveDomain(String dn)
+    {
+        try
+        {
+            InetAddress ip = InetAddress.getByName(dn);
+            System.out.println("Ip       :  "+ip.getHostAddress());
+        }
+        catch(Exception e)
+        {
+            System.out.print(e.getMessage());
         }
     }
 }
